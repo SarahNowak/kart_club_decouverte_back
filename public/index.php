a96eb4ec5f26d1ead606486653588d98dd545076
Sarah Nowak <nowaksarah44@gmail.com>
2022-01-19 17:19:37 +0100

    modif config

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@ use App\Kernel;
 
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token, Authorization');
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
